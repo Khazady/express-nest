4deffcbc1cc75665d4b4ccd32a769b7e977447b6
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2023-05-22 08:45:53 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from "express";
+
+const app = express()
+const port = 3002
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Changed Text! 234')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
